b87b7bed8d47ab10a7f174c0bbb6d9f73345d10d
David Augusto <daviaugusto222@gmail.com>
2020-04-28 11:27:14 -0300

    View Controller Recordações

---
+++ MaeCoruja/Controller/RecordacoesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RecordacoesViewController.swift
+//  MaeCoruja
+//
+//  Created by David Augusto on 28/04/20.
+//  Copyright © 2020 David Augusto. All rights reserved.
+//
+
+import UIKit
+
+class RecordacoesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
