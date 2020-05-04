6d711ddf682b3d6c1b2e8da9327f2034de748625
Felipe Santana <felipepipa16@hotmail.com>
2020-05-04 20:56:56 -0300

    Adicionados arquivos para uso do mapa

---
+++ MaeCoruja/Controller/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MaeCoruja
+//
+//  Created by Felipe Santana on 04/05/20.
+//  Copyright © 2020 David Augusto. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet private var mapView: MKMapView!
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+}
