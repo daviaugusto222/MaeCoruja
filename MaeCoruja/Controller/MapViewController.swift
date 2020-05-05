4d7800d356396da6f7acbdf35823721688c46d58
Felipe Santana <felipepipa16@hotmail.com>
2020-05-05 08:45:38 -0300

    Configuração de ponto inicial do mapa

--- MaeCoruja/Controller/MapViewController.swift
+++ MaeCoruja/Controller/MapViewController.swift
@@ -16,6 +16,28 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        //Define the Fortaleza latitude and longitude as initial
+        let initialLocation = CLLocation(latitude: -3.71839, longitude: -38.5434)
+        
+        //Center the map in Fortaleza
+        mapView.centerToLocation(initialLocation)
+        
     }
     
+}
+
+
+//Extension for set the zoom in a specified location
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
 }
