78c59ec1a92209afd5c539b249719d6dd7d8ee83
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-07 16:53:58 -0300

    Criando mock Conteudo

---
+++ MaeCoruja/Model/ComoDoar.swift
@@ -0,0 +1,20 @@
+//
+//  ComoDoar.swift
+//  MaeCoruja
+//
+//  Created by Kellyane Nogueira on 07/05/20.
+//  Copyright © 2020 David Augusto. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Conteudo{
+    let titles:Array<String> = []
+    let contents:Array<String> = []
+    
+    /*static func createConteudo() -> Conteudo {
+        return Conteudo(title: "", texto:"")
+    }*/
+}
