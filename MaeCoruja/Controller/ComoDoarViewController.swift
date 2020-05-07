49f1f470a9e7ac3be6f604561e8fbd53f77c2909
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-07 16:56:38 -0300

    Ajeitando um negocio

--- MaeCoruja/Controller/ComoDoarViewController.swift
+++ MaeCoruja/Controller/ComoDoarViewController.swift
@@ -12,8 +12,8 @@ class ComoDoarViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
     
-  //let titles:Array<String> = ["Como fazer a coleta do leite materno","Preparo do frasco para guardar o leite","Higiene pessoal antes de iniciar a coleta", "Local adequado para retirar o leite", "Como fazer para retirar o leite", "Como conservar o leite ordenhado", "Como oferecer o leite ordenhado à criança"]
-  // let contents:Array<String> = ["A maioria das mulheres que estão amamentando produz leite em excesso, especialmente do terceiro ao quinto dia após o parto. A produção do leite depende do esvaziamento da mama e, quanto mais a mulher esvazia as mamas, mais leite ela será capaz de produzir. Assim, as mães que desejam doar leite materno para ajudar na recuperação dos bebês hospitalizados, podem estocar o leite em vidros de café solúvel esterilizados e doá-los para os bancos de leite humano. O leite deve ser conservado no congelador por até 10 dias. E caso seja descongelado, não pode ser congelado novamente.","Conteúdo 2","Conteúdo 3", "Conteúdo 4", "Conteúdo 5", "Conteúdo 6", "Conteúdo 7"]
+  let titles:Array<String> = ["Como fazer a coleta do leite materno","Preparo do frasco para guardar o leite","Higiene pessoal antes de iniciar a coleta", "Local adequado para retirar o leite", "Como fazer para retirar o leite", "Como conservar o leite ordenhado", "Como oferecer o leite ordenhado à criança"]
+  let contents:Array<String> = ["A maioria das mulheres que estão amamentando produz leite em excesso, especialmente do terceiro ao quinto dia após o parto. A produção do leite depende do esvaziamento da mama e, quanto mais a mulher esvazia as mamas, mais leite ela será capaz de produzir. Assim, as mães que desejam doar leite materno para ajudar na recuperação dos bebês hospitalizados, podem estocar o leite em vidros de café solúvel esterilizados e doá-los para os bancos de leite humano. O leite deve ser conservado no congelador por até 10 dias. E caso seja descongelado, não pode ser congelado novamente.","Conteúdo 2","Conteúdo 3", "Conteúdo 4", "Conteúdo 5", "Conteúdo 6", "Conteúdo 7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,11 +46,11 @@ extension ComoDoarViewController: UITableViewDataSource, UITableViewDelegate{
             fatalError("Wrong identifier")
         }
         
-        //cell.labelTitle.text = titles[indexPath.row]
-        //cell.contentTextView.text = contents[indexPath.row]
-        //cell.imageView?.image = UIImage(named: titles[indexPath.row])
+        cell.labelTitle.text = titles[indexPath.row]
+        cell.contentTextView.text = contents[indexPath.row]
+        cell.imageView?.image = UIImage(named: titles[indexPath.row])
         
-        return //cell /
+        return cell
     }
     //IndexPath - Qual a seção e qual é a célula
 }
