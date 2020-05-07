40639265bd0149e2ed9cb3e6ae21dbbc6ddba7d3
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-07 12:41:13 -0300

    Ajustes no arquivos git

---
+++ MaeCoruja/View/Tela Conteudo de Guia/ConteudoGuiaTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ContentTableViewCell.swift
+//  Testes
+//
+//  Created by Kellyane Nogueira on 05/05/20.
+//  Copyright © 2020 Kellyane Nogueira. All rights reserved.
+//
+
+import UIKit
+
+class ConteudoGuiaTableViewCell: UITableViewCell {
+
+    static let nibname = "ConteudoGuiaTableViewCell"
+    static let identifier = "conteudoGuiaCell"
+    
+   
+   // override func prepareForReuse() {
+        //conteudo da celula
+   // }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    /*override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }*/
+
+}
